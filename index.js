7f586d370dc31168a0e40c1e5579072c5bf660ff
gabriel <gabrielmineironogueira@gmail.com>
2022-05-10 23:14:40 -0300

    entrega1

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ function somaProduto(productsCart){
 const h1     = document.createElement("h1")
 h1.innerHTML ="<strong>Papelaria</strong>"
 const button = document.createElement("button")
+button.innerHTML = "Finalizar"
 const main   = document.createElement("main")
 const li     = document.createElement("li")
 li.innerHTML = `${productsCart[0].nome}: R$${productsCart[0].preco} `
@@ -37,20 +38,29 @@ const li2    = document.createElement("li")
 li2.innerHTML= `${productsCart[2].nome}: R$${productsCart[2].preco}` 
 const li3    = document.createElement("li")
 li3.innerHTML= `${productsCart[3].nome}: R$${productsCart[3].preco}`
-const li4    = document.createElement("li")
-li4.innerHTML= `Total: R$${somaProduto(productsCart)}`
+
+const detalhes = [somaProduto(productsCart), button]
 const ul     = document.createElement("ul")
+//Button e total
+
+const ul2    = document.createElement("ul")
+const li4    = document.createElement("li")
+li4.appendChild(detalhes[1])
+const li5    = document.createElement("li")
+li5.innerHTML= `Total: R$${detalhes[0]}`
+
 ul.appendChild(li)
 ul.appendChild(li1)
 ul.appendChild(li2)
 ul.appendChild(li3)
-ul.appendChild(li4)
-ul.appendChild(button)
 main.appendChild(ul)
+ul2.appendChild(li4)
+ul2.appendChild(li5)
+ul.appendChild(ul2)
 document.querySelector("body").appendChild(main)
 document.querySelector("body").appendChild(h1)
-button.innerHTML = "Finalizar"
-
-const detalhes = [soma, button]
-
+li5.setAttribute("id", "total")
+li4.setAttribute("id", "finalizar")
+ul2.setAttribute("id", "lista2")
+console.log(main)
 
